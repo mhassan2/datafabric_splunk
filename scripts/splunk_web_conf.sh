575165d906bdadaa457019b5b20971df8bea98ca
mhassan2 <mhassan@splunk.com>
2017-12-22 10:27:41 -0600

    editing

--- scripts/splunk_web_conf.sh
+++ scripts/splunk_web_conf.sh
@@ -31,35 +31,47 @@ yarn_url="<a href=\"http://localhost:8088\" >http://localhost:8088</a>"
 mysql_url="n/a"
 kafka_url="n/a"
 zookeeper_url="n/a"
+tutorial_url="<font color=\"lime\" <a href=\"https://github.com/mhassan2/datafabric_splunk/wiki\" >[Click Here For Step By Step Exercises] </a></font>"
 
 touch /opt/splunk/etc/.ui_login	#prevent first time changeme password screen`
 /opt/splunk/bin/splunk edit user $USERADMIN -password $USERPASS -roles admin -auth admin:changeme
 
 #-------web.conf stuff-------
 LINE1="<CENTER><H2><font color=\"blue\"> &nbsp; Data Fabric Integration Sandbox &nbsp; </font></H2><br/></CENTER>"
-#useradmin
-LINE2="<CENTER><H3 style=\"text-align: left;\" <font color=\"white\">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; User: </font> <font color=\"red\">$USERADMIN</font> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; <font>Password:</font> <font color=\"red\"> $USERPASS</font></H3></font></CENTER>"
+
+#tutorial
+LINE2=" <H3 style=\"text-align: left;\"><font color=\"white\"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Tutorial: &nbsp;&nbsp;$tutorial_url </font></H3>"
+
 #hostname
-#docs: https://github.com/mhassan2/datafabric_splunk/wiki
 #LINE3=" <H3 style=\"text-align: left;\"><font color=\"#867979\"> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Hostname: </font><font color=\"#FF9033\"> $fullhostname &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; <font color=\"#867979\">  Host IP: </font><font color=\"#FF9033\"> $ip</font></H3>"
+
+#useradmin
+LINE3="<CENTER><H3 style=\"text-align: left;\" <font color=\"white\">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; User: </font> <font color=\"red\">$USERADMIN</font> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; <font>Password:</font> <font color=\"red\"> $USERPASS</font></H3></font></CENTER>"
+
 #supervisor
 LINE4=" <H3 style=\"text-align: left;\"><font color=\"#867979\"> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; SUPERVISOR:</font><font color=\"#FF9033\"> $SUPERVISOR_VER   &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; <font color=\"#867979\">  URL: </font><font color=\"#FF9033\"> $supervisor_url</font></H3>"
+
 #nifi
 LINE5=" <H3 style=\"text-align: left;\"><font color=\"#867979\"> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; NIFI:</font><font color=\"#FF9033\"> $NIFI_VER &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; <font color=\"#867979\">  URL: </font><font color=\"#FF9033\"> $nifi_url</font></H3>"
+
 #hadoop
 LINE6=" <H3 style=\"text-align: left;\"><font color=\"#867979\"> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; HADOOP:</font><font color=\"#FF9033\"> $HADOOP_VER  &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; <font color=\"#867979\">  URL: </font><font color=\"#FF9033\"> $hadoop_url</font></H3>"
+
 #yarn
 LINE7=" <H3 style=\"text-align: left;\"><font color=\"#867979\"> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; YARN:</font><font color=\"#FF9033\"> $YARN_VER &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; <font color=\"#867979\">  URL: </font><font color=\"#FF9033\"> $yarn_url</font></H3>"
+
 #kafka
 LINE8=" <H3 style=\"text-align: left;\"><font color=\"#867979\"> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; KAFAK:</font><font color=\"#FF9033\"> $KAFKA_VER  &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; <font color=\"#867979\">  URL: </font><font color=\"#FF9033\"> $kafka_url</font></H3>"
+
 #zk
 LINE9=" <H3 style=\"text-align: left;\"><font color=\"#867979\"> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; ZOOKEEPER:</font><font color=\"#FF9033\"> $ZOOKEEPER_VER   &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; <font color=\"#867979\">  URL: </font><font color=\"#FF9033\"> $zookeeper_url</font></H3>"
+
 #mysql
-LINE10=" <H3 style=\"text-align: left;\"><font color=\"#867979\"> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; MySQL:</font><font color=\"#FF9033\"> $MYSQL_VER  &nbsp;&nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; <font color=\"#867979\">  URL: </font><font color=\"#FF9033\"> $mysql_url</font></H3>"
+LINE10=" <H3 style=\"text-align: left;\"><font color=\"#867979\"> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; MySQL:</font><font color=\"#FF9033\"> $MYSQL_VER  &nbsp;&nbsp;&nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; <font color=\"#867979\">  URL: </font><font color=\"#FF9033\"> $mysql_url</font></H3>"
 
 
 #configure the custom login screen and http access for ALL (no exception)
-custom_web_conf="[settings]\nlogin_content=<div align=\"right\" style=\"border:2px solid green;\"> $LINE1 $LINE2 $LINE3 $LINE4 $LINE5 $LINE6 $LINE7 $LINE8 $LINE9 $LINE10 $LINE11 </div> <p><font color="\#867979\#"> Data Fabric Integration Sandbox v$CONTAINER_VER &nbsp; (container internal IP=$container_ip)</font></p>\n\nenableSplunkWebSSL=0\n"
+custom_web_conf="[settings]\nlogin_content=<div align=\"right\" style=\"border:2px solid green;\"> $LINE1 $LINE2 $LINE3 $LINE4 $LINE5 $LINE6 $LINE7 $LINE8 $LINE9 $LINE10 $LINE11 </div> <p><font color="\#867979\#"  <a href=\"https://github.com/mhassan2/datafabric_splunk\"  >https://github.com/mhassan2/datafabric_splunk </a>  &nbsp;&nbsp;&nbsp; (container internal IP=$container_ip) </font></p> \n\nenableSplunkWebSSL=0\n"
 
 printf "$custom_web_conf" > /tmp/web.conf
 cp /tmp/web.conf /opt/splunk/etc/system/local/web.conf
